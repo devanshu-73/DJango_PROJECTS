90374a86fc8198d27b3a577b6ae9e79d5f299c06
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-05 20:12:07 +0530

    ....

---
+++ Digital_Society/project/app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique= True,max_length= 30)
+    password = models.CharField(max_length= 20)
+    role = models.CharField(max_length= 20) # chairman or society_member
+    isActive = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now = True)
+
+class Chairman(models.Model):
+    userid = models.ForeignKey(User,on_delete = models.CASCADE)
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length = 10)
+    contact = models.CharField(max_length = 10)
+    blockno = models.CharField(max_length = 3)
+    houseno = models.CharField(max_length = 4)
+    
+    
---
+++ Digital_Society/project/chairman/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChairmanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chairman'
