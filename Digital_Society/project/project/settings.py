7395c71075f7b2b30804f4baa304d0ff61498339
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-09 18:51:27 +0530

    Digital_Society

--- Digital_Society/project/project/settings.py
+++ Digital_Society/project/project/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_ROOT = os.path.join('media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT = os.path.join('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
