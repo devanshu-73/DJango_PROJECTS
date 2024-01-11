fa845a71504595dcfa4c538283ad26deb3d2afb5
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-11 19:14:33 +0530

    Digital Society

--- Digital_Society/project/app/admin.py
+++ Digital_Society/project/app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email','role','isActive','created_at')
+
+class ChairmanAdmin(admin.ModelAdmin):
+    list_display = ('firstname','lastname')
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('firstname','lastname','contact','blockno','houseno')
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Chairman)
+admin.site.register(User,UserAdmin)
+admin.site.register(Chairman,ChairmanAdmin)
+admin.site.register(Member,MemberAdmin)
--- Digital_Society/project/app/models.py
+++ Digital_Society/project/app/models.py
@@ -7,9 +7,6 @@ class User(models.Model):
     role = models.CharField(max_length= 20) # chairman or society_member
     isActive = models.BooleanField(default = False)
     created_at = models.DateTimeField(auto_now = True)
-    
-    def __str__(self):
-        return (self.role +" "+ self.email) 
 
 class Chairman(models.Model):
     userid = models.ForeignKey(User,on_delete = models.CASCADE)
@@ -18,8 +15,16 @@ class Chairman(models.Model):
     contact = models.CharField(max_length = 10)
     blockno = models.CharField(max_length = 3)
     houseno = models.CharField(max_length = 4)
-    
-    def __str__(self):
-        return (self.firstname +" | "+"Block : "+self.blockno+" | "+"House No : "+self.houseno) 
-    
-    
+class Member(models.Model):
+    userid = models.ForeignKey(User,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length=20)
+    contact = models.CharField(max_length=11)
+    blockno = models.CharField(max_length=3)
+    houseno = models.CharField(max_length=4)
+    occupation = models.CharField(max_length=20)
+    job_address = models.TextField()
+    bloodgroup = models.CharField(max_length=3)
+    vehical_details = models.CharField(max_length=20)
+    pic = models.FileField(upload_to="static/images/doctors/",default="member6.png")
+
