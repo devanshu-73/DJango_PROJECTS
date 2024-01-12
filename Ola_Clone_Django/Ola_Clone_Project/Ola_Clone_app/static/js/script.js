f7be5b48acb13039e04fb78cf5f271a2722f12a4
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-12 18:52:12 +0530

    Ola_Clone_Project

---
+++ Ola_Clone_Django/Ola_Clone_Project/Ola_Clone_app/static/js/script.js
@@ -0,0 +1,9 @@
+      // sec-6 -> jQuery to toggle image containers
+      $(document).ready(function () {
+        $('.image-container:first').addClass('active');
+    });
+
+    function toggleImages(itemIndex) {
+        $('.image-container').removeClass('active');
+        $('.image-container').eq(itemIndex).addClass('active');
+    }
