1be854b2aee2f4f17a58b4c3c6175fdf513ed871
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-13 22:35:32 +0530

    Digital Society

--- Digital_Society/project/app/views.py
+++ Digital_Society/project/app/views.py
@@ -37,31 +37,129 @@ def login (request):
                 uid = User.objects.get(email = p_email,password = p_password)
                 cid = Chairman.objects.get(userid = uid)
                 request.session['email']=uid.email
-                print("------------------>>>>  Uid :",uid)
                 return HttpResponseRedirect(reverse('home'))
             
                 # print("------------------>>>>  Password :",p_password)
                 # print("------------------>>>>  Email :",p_email)
+                # print("------------------>>>>  Uid :",uid)
                 # print("------------------>>>>  FirstName :",cid.firstname)
                 # print("------------------>>>>  LastName :",cid.lastname)
-                
                 # return index(request) # self practice...
-            except:
+            except Exception as e:
                 msg = "Invalid Email Or Password"
                 return render(request,'app/login.html',{'e_msg':msg})
                 # return home(request,msg=msg)
         else:
-            print("------------------>>>>  Page Loaded")
+            # print("------------------>>>>  Page Loaded")
             return render(request,'app/login.html')
             
 def logout(request):
     if 'email' in request.session:
         del request.session['email']
-        # return render(request,'app/login.html')
         return HttpResponseRedirect(reverse('login'))
+        # return render(request,'app/login.html')
     else:
         return HttpResponseRedirect(reverse('login'))
 
+def profile(request):
+    if 'email' in request.session:
+        uid = User.objects.get(email = request.session['email'])
+        cid = Chairman.objects.get(userid = uid)
+        context = {
+            'uid' : uid,
+            'cid' : cid,
+        }
+        return render(request,'app/profile.html',context)
+    else:
+        return HttpResponseRedirect(reverse('login'))
+    
+
+def add_member(request):
+    if "email" in request.session:
+        uid = User.objects.get(email = request.session['email'])
+        cid = Chairman.objects.get(userid = uid)
+
+        if request.POST:
+            print("---> add operation")
+            email = request.POST['email']
+            contact = request.POST['contact']
+            firstname = request.POST['firstname']
+            l1 = ["2w334d","43rsd","9as23","9ds03","ds832d"]
+            password = random.choice(l1)+email[3:6]+contact[4:7]
+            muid = User.objects.create(email = request.POST['email'],password = password,role="member") 
+            if muid:
+                mid = Member.objects.create(
+                                userid = muid,
+                                firstname = request.POST['firstname'],
+                                lastname = request.POST['lastname'],
+                                contact = request.POST['contact'],
+                                blockno = request.POST['blockno'],
+                                houseno = request.POST['houseno'],
+                                occupation = request.POST['occupation'],
+                                job_address = request.POST['job_address'],
+                                bloodgroup = request.POST['bloodgroup'],
+                                vehical_details = request.POST['vehical_details']
+                        )
+                if mid:
+                    mymailfunction("Welcome to Digital Society","mymailtemplate",email,{'firstname' : firstname,'password':password})                    
+                context = {
+                    'uid' : uid,
+                    'cid' : cid,
+                    's_msg' : "successfully Member added"
+                    }
+                return render(request,"app/addMember.html",context)
+                
+
+        context = {
+                'uid' : uid,
+                'cid' : cid,
+        }
+        return render(request,"app/addMember.html",context)
+ 
+
+def change_password(request):
+    if "email" in request.session:
+        uid = User.objects.get(email = request.session['email'])
+        cid = Chairman.objects.get(userid = uid)
+        
+        currentpassword = request.POST['currentpassword']
+        newpassword = request.POST['newpassword']
+        if uid.password == currentpassword:
+            uid.password = newpassword
+            uid.save() # update 
+        
+            del request.session['email']
+            s_msg = "Successfully Password Changed"
+            return render(request,"app/login.html",{'s_msg':s_msg})
+        else:
+            e_msg = "Invalid current password"
+            del request.session['email']
+            return render(request,"app/login.html",{'e_msg':e_msg})
+        
+def change_profile(request):
+    if "email" in request.session:
+        uid = User.objects.get(email = request.session['email'])
+        cid = Chairman.objects.get(userid = uid)
+        
+        if request.POST:
+            cid.firstname = request.POST['firstname']
+            cid.lastname =  request.POST['lastname']
+            cid.contact =request.POST['contact']
+            cid.houseno = request.POST['houseno']
+            cid.blockno = request.POST['blockno']
+            if "pic" in request.FILES:
+                cid.pic = request.FILES['pic']
+                cid.save()
+            cid.save() # update
+
+        context = {
+                'uid' : uid,
+                'cid' : cid,
+        }
+        return render(request,"app/profile.html",context)
+       
+        
+    
 # ============================================================================================
 # ============================================================================================
 # ------------------------------------------------
