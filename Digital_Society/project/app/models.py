bc785cf2659a11987eeb2b70425676c7dcc45b6b
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-16 19:22:03 +0530

    Digital Society

--- Digital_Society/project/app/models.py
+++ Digital_Society/project/app/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(unique= True,max_length= 30)
-    password = models.CharField(max_length= 20)
-    role = models.CharField(max_length= 20) # chairman or society_member
+    email = models.EmailField(unique=True,max_length=30)
+    password = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)  #chairman / societymember
     isActive = models.BooleanField(default = False)
-    created_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now=True)
+
 class Chairman(models.Model):
-    userid = models.ForeignKey(User,on_delete = models.CASCADE)
+    userid = models.ForeignKey(User,on_delete=models.CASCADE)
     firstname = models.CharField(max_length = 20)
-    lastname = models.CharField(max_length = 10)
-    contact = models.CharField(max_length = 10)
-    blockno = models.CharField(max_length = 3)
-    houseno = models.CharField(max_length = 4)
+    lastname = models.CharField(max_length=20)
+    contact = models.CharField(max_length=11)
+    blockno = models.CharField(max_length=3)
+    houseno = models.CharField(max_length=4)
+    pic = models.FileField(upload_to="media/upload",default="default.png")
+
 class Member(models.Model):
     userid = models.ForeignKey(User,on_delete=models.CASCADE)
     firstname = models.CharField(max_length = 20)
@@ -26,3 +29,6 @@ class Member(models.Model):
     bloodgroup = models.CharField(max_length=3)
     vehical_details = models.CharField(max_length=20)
     pic = models.FileField(upload_to="media/upload",default="default.png")
+
+
+    
--- Digital_Society/project/project/settings.py
+++ Digital_Society/project/project/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-# MEDIA_ROOT = os.path.join('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
