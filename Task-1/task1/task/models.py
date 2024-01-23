c414f05af6258e493ff9de86be04fc959d003199
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-23 16:46:58 +0530

    Task1

--- Task-1/task1/task/models.py
+++ Task-1/task1/task/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class User_details(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=25)
     subjects = models.JSONField(default=list)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(max_length=15)
  
--- Task-1/task1/task/urls.py
+++ Task-1/task1/task/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('task.urls') ),
-   
+    path('', views.dashboard, name='dashboard'),
+    path('delete-user/<int:srno>', views.delete_user, name='delete-user'),
+    path('update-user/<int:srno>', views.update_user, name='update-user'),
 ]
+   
