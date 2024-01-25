d18d852f441e35a72527389c8bd90f092b56cdc2
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-25 19:39:00 +0530

    ..

--- Django_01/app_01/app_01/views.py
+++ Django_01/app_01/app_01/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 def firstPage(request):
     # return HttpResponse("<h1>Views : First Page</h1>")
     context={
-        'page':"First Page"
+        
     }
     return render(request,"first.html",context)
         
 def secondPage(request):
-    context={'page':"Second Page"}
+    context={'page':}
     return render(request,"second.html",context)
     # return HttpResponse("<h1>Views : Second Page</h1>")
 
 def thirdPage(request):
-    context={'page':"Third Page"}
+    context={'page':["First Page","Second Page","Third Page"]}
     return render(request,"third.html",context)
     # return HttpResponse("<h1>Views : Third Page</h1>")
