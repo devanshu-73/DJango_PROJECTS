cd3fe132986d7d48eb74cadb59e9aa3630ad9e56
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-30 19:36:48 +0530

    ...

--- Django_API/Api_Project/Api_App/views.py
+++ Django_API/Api_Project/Api_App/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# # Create your views here.
-# def index(request):
-#     pass
+# Create your views here.
+def index(request):
+    pass
