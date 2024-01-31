0e3d1ff7aad90c154c20f89a12e1575c2b4aa59a
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-31 19:31:03 +0530

    ...

--- Django_API/Api_Project/Api_Project/urls.py
+++ Django_API/Api_Project/Api_Project/urls.py
@@ -1,13 +1,3 @@
-"""
-URL configuration for Api_Project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-
 from django.contrib import admin
 from django.urls import path
 
