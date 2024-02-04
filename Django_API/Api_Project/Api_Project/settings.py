3bc8c6f1a30edb6b61b614aa2670c16814b78672
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-04 20:55:24 +0530

    Api_Task

--- Django_API/Api_Project/Api_Project/settings.py
+++ Django_API/Api_Project/Api_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Api_App',
 ]
 
 MIDDLEWARE = [
