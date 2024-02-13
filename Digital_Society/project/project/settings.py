1f3ee59cf5373b39bbff829c1713750fd0cd5675
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-13 12:59:55 +0530

    digital society

--- Digital_Society/project/project/settings.py
+++ Digital_Society/project/project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    # 'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django_API/Api_Project/Api_App/views.py
+++ Django_API/Api_Project/Api_App/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>lllll</h1>")
+    return render(request,'index.html')
