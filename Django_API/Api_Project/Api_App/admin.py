7e5c8bb7c499560ebbb121d3f9bbb618aad4e7e4
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-14 20:20:10 +0530

    ...

--- Django_API/Api_Project/Api_App/admin.py
+++ Django_API/Api_Project/Api_App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('name','email','password','isActive','created_at')
+
 
 # Register your models here.
+admin.site.register(Form, FormAdmin)
+    
--- Django_API/Api_Project/Api_App/views.py
+++ Django_API/Api_Project/Api_App/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import *
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        form_data = Form.objects.create(
+            name = name,
+            email = email,
+            password =  password,
+        )
+        return render(request,'index.html',context)
+    else:
+        users = Form.objects.all()
+        context = {'users':users}
+        return render(request,'index.html',context)
+
+# def data(request):
+    # return render(request,'index.html')
