7d218ea46fd44d1f6f90854c0d965c22c9ba4848
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-14 19:30:12 +0530

    Api

--- Django_API/Api_Project/Api_App/models.py
+++ Django_API/Api_Project/Api_App/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Form(models.Model):
-    id = models.AutoField
-    
+    name = models.CharField(max_length = 30)
+    email = models.EmailField(unique = True,max_length = 50)
+    password = models.CharField(max_length = 20)
+    isActive = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now = True)
 
+    def __str__(self):
+        return self.name
+    
